b81c253a3f83ee0a1eaac1563470abfd15b7fdd9
JackSparring420 <emanuele.arconte94@gmail.com>
2021-11-03 17:03:37 +0100

    work in progress

--- myscript.js
+++ myscript.js
@@ -3,7 +3,11 @@
 // 2 Creo una variabile in cui riporto la funzione
 // 3 Metto a confronto i numeri dell'array con le posizoni dei singoli quadrati
     // 3.1 se corrisponde aggiungo la classe bomb per cambiare il colore del quadrato
-// 4 Porto la cdlasse bomb su tette le mine per scoprirle tutte
+// 4 Porto la classe hit su tette le mine per scoprirle tutte
+    // 4.1 creo una classe marcatrice per segnare i volori dell'array(delle bombe)
+    // 4.2 genero una variabile che va a selezionare tutti i valori marcari
+    // 4.3 con un ciclo for aggiungo la classe hit a tutti gli elementi marcati
+// 5 Conto quanti clicchi fa l'utente
 
 
 const gridCont = document.getElementById("grid");
@@ -13,7 +17,6 @@ let livello = prompt("Scegli: facile, medio, difficile");
 while ((livello !== "facile") && (livello !== "medio") && (livello !=="difficile")){
     livello = prompt("Scegli: facile, medio, difficile");
 }
-
 
 
 let dimensione;
@@ -36,26 +39,34 @@ for(let i = 1; i<= dimensione; i++) {
     newElement.classList.add(difficolta)
     newElement.classList.add("square")
     
-    let found = false;
-
+    if (bomb.includes(i)) {
+        newElement.classList.add('bomb');
+    }
+    
     newElement.innerText += i;
+    
     newElement.addEventListener("click",
-    function() {
+    function contatore() {
         this.classList.add("click-true");
-                    
-            for (let a = 0; a <= bomb.length ; a++){
-                if (i === bomb[a]) {
-                    found = true;
-                }
-
-            }
-                
-            if (found === true) {
-                    this.classList.add("bomb");
+        let found = false;
+        for (let a = 0; a <= bomb.length ; a++){
+            if (i === bomb[a]) {
+                found = true;
             }
         }
         
-    )
+        if (found === true) {
+            this.classList.add("hit");
+            // Il giocatore ha perso
+            // Devo trovare tutte le altre bombe
+            const bombs = document.getElementsByClassName('bomb');
+            for (let j = 0; j < bombs.length; j++) {
+                bombs[j].classList.add('hit');
+            }
+            
+        }
+        console.log(i);
+    });
     
     gridCont.appendChild(newElement)
 }
@@ -66,30 +77,35 @@ for(let i = 1; i<= dimensione; i++) {
     // let node= document.createElement(elementGen);
     // node.classList.add(classPlus)
     // node.classList.add(classPlusB)
-//     return node
-// }
-
-function createBombs(numberSquare){
-
-    let bombs =[];
-
-    for (let i = 0; i < 16; i ++){
+    //     return node
+    // }
+    
+    function createBombs(numberSquare){
         
-        let randombomb = Math.floor(Math.random() * numberSquare) + 1;
+        let bombs =[];
+        
+        for (let i = 0; i < 16; i ++){
+            
+            let randombomb = Math.floor(Math.random() * numberSquare) + 1;
+            
+            // let trovato = false;
+            // if (randombomb === bombs[i]){
+                //     trovato = true;
+                // }
+                // if (trovato == false){
+                    //     bombs.push(randombomb);
+                    // }
+                    if (bombs.includes(randombomb)) {
+                        i--;
+                    }  else {
+                        bombs.push(randombomb);
+                    }         
+                }
+                console.log(bombs); 
+                return bombs
+            }
 
-        // let trovato = false;
-        // if (randombomb === bombs[i]){
-        //     trovato = true;
-        // }
-        // if (trovato == false){
-        //     bombs.push(randombomb);
-        // }
-        if (bombs.includes(randombomb)) {
-            i--;
-        }  else {
-            bombs.push(randombomb);
-        }         
-    }
-    console.log(bombs); 
-    return bombs
-}
+            var counterVal = 0;
+            function incrementClick() {
+                updateDisplay(++counterVal);
+            }
